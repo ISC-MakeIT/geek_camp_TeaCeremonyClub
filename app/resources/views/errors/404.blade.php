03102e0d09fd6c34b7fd399465f14a058e053d8a
sol <ocosio0907@gmail.com>
2023-08-20 09:55:37 +0900

    fix image path

--- app/resources/views/errors/404.blade.php
+++ app/resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
     <section class="error-page">
         <h1 class="error-page-title">This url is not found.<br> The cat is realy coooooooool DA nya~.</h1>
 
-        <img class="error-page-img" src="/cat.png" alt="CAT">
+        <img class="error-page-img" src="/images/cat.png" alt="CAT">
 
         <a class="error-page-link" href="/">トップ画面へ戻る</a>
     </section>
